dd93aef2e8dd0204cd266d8056941b4f2d5098e0
nhatvt123 <tuannhatfpt@gmail.com>
2022-05-30 19:47:53 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	printf("Welcome to C!\n");
+	return 0;
+}
