d7088c0d15e846b0362587d9bd8eabefbb370f83
nhatvt123 <tuannhatfpt@gmail.com>
2022-05-30 20:50:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	printf("Welcome to C!\n");
-	printf("demo");
+	int integer1;
+	int integer2;
+	int sum;
+	
+	printf("Enter first interger \n");
+	scanf("%d", &integer1);
+	printf("Enter second interger\n");
+	scanf("%d", &integer2);
+	sum = integer1 + integer2;
+	printf("sum=%d",sum);
 	return 0;
 }
