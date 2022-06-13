29b96c3248274d2ba5b045a8ca58fd3fcef95474
nhatvt123 <tuannhatfpt@gmail.com>
2022-06-13 18:21:03 +0700

    task7

---
+++ task6.11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() 
+{
+	int num, res;
+	printf("nhap so can kiem tra");
+	scanf("%d", num);
+	
+	res = num % 2;
+	if( res ==0)
+	 printf(" so chan");
+	else
+	 printf("so le");
+
+	 
+}
